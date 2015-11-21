db1dca7d4e893b981e51c95dc5ae988a0845f608
Otavio <otaviojava@java.net>
2015-11-21 17:20:15 -0200

    Adiciona qualificador de moeda

---
+++ javaee7/src/main/java/org/javamoney/midas/javaee7/cdi/CurrencyQualifier.java
@@ -0,0 +1,18 @@
+package org.javamoney.midas.javaee7.cdi;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Target( { METHOD, FIELD, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Qualifier
+public @interface CurrencyQualifier {
+	String currency() default "";
+}
