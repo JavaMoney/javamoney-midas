8e7132d22995abe0ba9221081546fa9311948526
Otavio <otaviojava@java.net>
2015-11-21 17:24:16 -0200

    Adds converters to jaxrs

---
+++ javaee7/src/main/java/org/javamoney/midas/javaee7/jaxrs/CurrencyUnitConverterProvider.java
@@ -0,0 +1,39 @@
+package org.javamoney.midas.javaee7.jaxrs;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.money.CurrencyUnit;
+import javax.money.Monetary;
+import javax.ws.rs.ext.ParamConverter;
+import javax.ws.rs.ext.ParamConverterProvider;
+import javax.ws.rs.ext.Provider;
+
+
+@Provider
+public class CurrencyUnitConverterProvider implements ParamConverterProvider {
+
+    @Override
+    public <T> ParamConverter<T> getConverter(Class<T> rawType, Type genericType, Annotation[] annotations) {
+        if (CurrencyUnit.class.isInstance(rawType)) {
+            return new ParamConverter<T>() {
+                @Override
+                public T fromString(String value) {
+                    if(value == null || value.isEmpty()) {
+                        return null;
+                    }
+                    return rawType.cast(Monetary.getCurrency(value));
+                }
+
+                @Override
+                public String toString(T value) {
+                    if(value == null) {
+                        return null;
+                    }
+                    return value.toString();
+                }
+            };
+        }
+            return null;
+    }
+}
---
+++ javaee7/src/main/java/org/javamoney/midas/javaee7/jaxrs/MonetaryAmountConverterProvider.java
@@ -0,0 +1,40 @@
+package org.javamoney.midas.javaee7.jaxrs;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.money.MonetaryAmount;
+import javax.ws.rs.ext.ParamConverter;
+import javax.ws.rs.ext.ParamConverterProvider;
+import javax.ws.rs.ext.Provider;
+
+import org.javamoney.moneta.Money;
+
+@Provider
+public class MonetaryAmountConverterProvider implements ParamConverterProvider {
+
+
+    @Override
+    public <T> ParamConverter<T> getConverter(Class<T> rawType, Type genericType, Annotation[] annotations) {
+        if (MonetaryAmount.class.isInstance(rawType)) {
+            return new ParamConverter<T>() {
+                @Override
+                public T fromString(String value) {
+                    if(value == null || value.isEmpty()) {
+                        return null;
+                    }
+                    return rawType.cast(Money.parse(value));
+                }
+
+                @Override
+                public String toString(T value) {
+                    if(value == null) {
+                        return null;
+                    }
+                    return value.toString();
+                }
+            };
+        }
+        return null;
+    }
+}
